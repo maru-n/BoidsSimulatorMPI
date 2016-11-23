7c995daaee67ce2910176699c2e310b3085d025b
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2016-11-23 20:37:01 +0900

    no message

---
+++ job_scripts/example_mpi.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+#
+#PJM --rsc-list "node=2x2x2"
+#PJM --rsc-list "elapse=1:00:00"
+#PJM --mpi "rank-map-bynode=XYZ"
+#PJM --mpi "use-rankdir"
+#PJM --stg-transfiles all
+#PJM --stgin "rank=* ./MassiveSwarmSimCUI_MPI %r:./"
+#PJM --stgin "rank=* ./settings/example.ini %r:./"
+#PJM --stgout "rank=0 %r:./data*.ptcl /data/hp160264/k03378/"
+#PJM -s
+#PJM --name testjob_mpi
+#
+
+. /work/system/Env_base
+
+export AGENT_NUM=10000
+export TIME_STEP=30
+export DATA_FILE_NAME=data_${PJM_JOBID}.ptcl
+
+mpiexec ./MassiveSwarmSimCUI_MPI ${DATA_FILE_NAME} ${AGENT_NUM} ${TIME_STEP} example.ini
+#mpiexec –mca mpi_print_stats 1 ./MassiveSwarmSimCUI_MPI ${DATA_FILE_NAME} ${AGENT_NUM} ${TIME_STEP} *.ini
