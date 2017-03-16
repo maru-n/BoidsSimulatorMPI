adf0cacc542f7ca6d020e82d37381a8e54e9ff7b
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2017-03-16 16:14:54 +0900

    add test code for MPI and spliced calculation.

--- test/compare_mpi.sh
+++ test/compare_mpi.sh
@@ -7,22 +7,21 @@ export setting_file="../../settings/test.ini"
 export CMAKE_OPTIONS="-D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++"
 
 export data_name=`mktemp`
-export data_name_legacy=`mktemp`
+export data_name_mpi=`mktemp`
 
 echo -e "build..."
 if [ ! -e ${build_dir} ]; then mkdir ${build_dir} ; fi
 cd ${build_dir}
 cmake ${CMAKE_OPTIONS} ../../ || exit -1
-make boidsim boidsim_legacy || exit -1
+make boidsim boidsim_mpi || exit -1
 
 echo -e "calculating normal version..."
-./boidsim -s ${setting_file} -o ${data_name}_org -T $T > /dev/null || exit -1
-dd if=${data_name}_org bs=1 skip=50 of=${data_name} 2> /dev/null
+./boidsim -s ${setting_file} -o ${data_name} -T $T > /dev/null || exit -1
 
-echo -e "calculating legacy version..."
-./boidsim_legacy ${data_name_legacy} $N $T > /dev/null || exit -1
+echo -e "calculating MPI version..."
+mpiexec -n 8 ./boidsim_mpi -s ${setting_file} -o ${data_name_mpi} -T $T > /dev/null || exit -1
 
-cmp ${data_name} ${data_name_legacy} 0 0 > /dev/null
+cmp ${data_name} ${data_name_mpi} 0 0> /dev/null
 if [ $? = 0 ]; then
     echo -e "\033[0;32mTest Succeeded\033[0;39m"
 else
---
+++ test/compare_split.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+export N=1000
+export T=30
+export build_dir=build
+export setting_file="../../settings/test.ini"
+export CMAKE_OPTIONS="-D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++"
+
+export data_name=`mktemp`
+export data_name_split=`mktemp`
+export data_name_split_first=`mktemp`
+export data_name_split_last=`mktemp`
+
+echo -e "build..."
+if [ ! -e ${build_dir} ]; then mkdir ${build_dir} ; fi
+cd ${build_dir}
+cmake ${CMAKE_OPTIONS} ../../ || exit -1
+make boidsim || exit -1
+
+echo -e "calculating normal version..."
+./boidsim -s ${setting_file} -T $T -o ${data_name} > /dev/null || exit -1
+
+echo -e "calculating split version..."
+t_first=$(($T/2))
+t_last=$(( $T-$t_first ))
+./boidsim -s ${setting_file} -T $t_first -o ${data_name_split_first} > /dev/null || exit -1
+./boidsim -s ${setting_file} -T $t_last  -i ${data_name_split_first} -o ${data_name_split} > /dev/null || exit -1
+
+cmp ${data_name} ${data_name_split} 0 0> /dev/null
+if [ $? = 0 ]; then
+    echo -e "\033[0;32mTest Succeeded\033[0;39m"
+else
+    echo -e "\033[0;31mTest Failed\033[0;39m"
+fi
+
+exit 0
