764050c55e4a966c82423097a48039b7bba021b6
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2017-04-27 02:40:12 +0900

    no message

--- test/execution_time.sh
+++ test/execution_time.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 export N=1000
-export T=30
+export T=50
 export build_dir=build
-export setting_file="../../settings/test.ini"
+export setting_file="../../settings/mototake.ini"
 export CMAKE_OPTIONS="-D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++"
 
 export data_name=`mktemp`
@@ -16,16 +16,9 @@ cmake ${CMAKE_OPTIONS} ../../ || exit -1
 make boidsim boidsim_mpi || exit -1
 
 echo -e "calculating normal version... =>" ${data_name}
-./boidsim -s ${setting_file} -o ${data_name} -T $T > /dev/null || exit -1
+time ./boidsim -s ${setting_file} -o ${data_name} -T $T > /dev/null || exit -1
 
 echo -e "calculating MPI version... =>" ${data_name_mpi}
-mpiexec -n 8 ./boidsim_mpi -s ${setting_file} -o ${data_name_mpi} -T $T > /dev/null || exit -1
-
-cmp ${data_name} ${data_name_mpi} 0 0> /dev/null
-if [ $? = 0 ]; then
-    echo -e "\033[0;32mTest Succeeded\033[0;39m"
-else
-    echo -e "\033[0;31mTest Failed\033[0;39m"
-fi
+time mpiexec -n 8 ./boidsim_mpi -s ${setting_file} -o ${data_name_mpi} -T $T > /dev/null || exit -1
 
 exit 0
