bfa11bb98f517f45baad46e4ad601331851f9d33
Maruyama Norihiro <norihiro.maruyama@gmail.com>
2018-05-10 01:09:38 +0900

    no message

---
+++ job_scripts/template_mpi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+#
+#PJM --rsc-list "node={{node}}"
+#PJM --rsc-list "elapse={{elapse}}"
+#PJM --stg-transfiles all
+#PJM --stgin "rank=* ./boidsim_mpi %r:./"
+#PJM --stgin "rank=* ../settings/*.ini %r:./"
+#PJM --stgout "rank=0 ./*.ptcl /data/hp160264/k03378{{data_safix}}/"
+#PJM --mpi "rank-map-bynode=XYZ"
+#PJM --mpi "use-rankdir"
+#PJM -s
+#PJM --name {{name}}
+#
+
+. /work/system/Env_base_1.2.0-20-1
+export LD_LIBRARY_PATH=/opt/rist/boost-1.53.0/lib:$LD_LIBRARY_PATH
+
+export TIME_STEP={{time_step}}
+export AGENT_NUM={{agent_num}}
+export FIELD_SIZE={{field_size}}
+export DATA_FILE_NAME={{name}}_${PJM_JOBID}.ptcl
+
+mpiexec ./boidsim_mpi -s {{setting}} -T ${TIME_STEP} -N ${AGENT_NUM} -F ${FIELD_SIZE} -o ${DATA_FILE_NAME}
+#mpiexec -mca mpi_print_stats 1 ./boidsim_mpi -s example.ini -T ${TIME_STEP} -N ${AGENT_NUM} -F ${FIELD_SIZE} -o ${DATA_FILE_NAME}
